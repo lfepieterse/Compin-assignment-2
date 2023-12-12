0389e2e5865dcad7e160fecfb63317a2fd7af847
RobinvanGelooven <r.m.h.vangelooven@students.uu.nl>
2023-12-12 18:34:35 +0100

    OKe, deze code is nog niet af maar we komen er wel! Het wordt ff debuggen om zeker te zijn dat de evaluatie formule klopt :)

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
-using random;
-
 
 //Evaluatie functie schrijven
 //Klasse blok maken
@@ -16,7 +14,17 @@ namespace MyApp
         public Nummer[,] Cel = new Nummer[3, 3];
         // Implementeer methoden om met de cijfers in het blok te werken
     }
+    internal class Nummer
+    {
+        public int Getalwaarde { get; set; }
+        public bool Locked { get; set; }
+        public Nummer(int Getal, bool JaOfNee) {
 
+            Getalwaarde = Getal;
+            Locked = JaOfNee;
+
+    }
+}
     internal class Program
     {
         private static Random random = new Random();
@@ -28,7 +36,7 @@ namespace MyApp
 
             //string input = Console.ReadLine(); //Ervan uitgaande dat er input is
 
-            //-----BLOKKEN AANMAKEN---
+            //-----BLOKKEN AANMAKEN en INVULLEN---
             Blok[,] Grid = new Blok[3, 3];
 
             for (int i = 0; i < 3; i++)
@@ -36,97 +44,150 @@ namespace MyApp
                 for (int j = 0; j < 3; j++)
                 {
                     Grid[i, j] = MaakBlok(input, i * 3, j * 3); //Hiermee maken we de blokken aan.
-                    BlokInVuller(Grid[i, j]); //moet andere naam krijgen
+                    BlokInVuller(Grid[i, j]); //En zo vullen we het blok meteen in!
                 }
             }
+            // KLAAR MET BLOKKEN AANMAKEN EN INVULLEN
 
-            //---BLOKKEN INVULLEN---
+            //EVALUATIE LIJST AANMAKEN
 
+            int[] Totaleduplicatenwaardenlijst = new int[18];
+            int Teller = 0;
 
-               // Nu kun je individuele blokken invullen en bewerken
-        // blokken[0, 0] is het eerste blok bijvoorbeeld.
+            for (int rijindex = 0; rijindex<9;rijindex++) 
+            {
+                Totaleduplicatenwaardenlijst[Teller] = EvalueerRij(Grid,rijindex);
+                Teller++;
+            }
+
+            for (int kolomindex = 0; kolomindex<9;kolomindex++) //Kolomindex + 9 = lijstindex
+            {
+                Totaleduplicatenwaardenlijst[Teller] = EvalueerKolom(Grid,kolomindex);
+                
+                Teller++;
+            }
+
+            //EVALUATIE LIJST AANGEMAAKT!!
+
+            //NU: pak blok, ga alle getallen swappen en check
+            
+            string Yeehaw = Console.ReadLine(); //random line for fun en voor debuggging
+
+            //NU willen we een heuristieke functie maken, die aan de hand van een totaal grid en rij en kolom aanduiding de heur waarde kan 
+            //berekenen.
 
 
         }
-        static void BlokInVuller(Blok blok)
-    {
-        List<int> beschikbareGetallen = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 }; //alle getallen van 0 t/m 9!
-        //Eerst gaan we de getallen eruit halen die al locked zijn. 
-        List<int> verbodencijfers = new List<int>();
-
-        //Eerst zorgen we dat de getallen van de locked cellen niet gebruikt gaan worden. 
-        for (int i = 0; i < 3; i++)
+        static int EvalueerRij(Blok[,] grid, int rijIndex)
         {
-            for (int j = 0; j < 3; j++)
-            {
-                if (blok.Cel[i,j].Locked == true) {
-                    verbodencijfers.Add(blok.Cel[i,j].Getalwaarde); //hiermee maken we een lijst aan getallen die verboden zijn en dus niet mogen worden ingevuld!
-                }
-            }
-        }
-        List<int> GoedeLijstMeTMinderGetallen = beschikbareGetallen.Except(verbodencijfers).ToList();
+            List<int> Waarden = new List<int>();
+            int duplicaten = 0;
+            int BlockRowIndex;
+            //Nu willen we een lijst maken van alle getallen in de verschillende blokken, in dezelfde kolommen
+            
+            //BlockRowIndex bepalen: op welke rij (vd blokken, dus 1e, 2e of 3e) moeten wij gaan zoeken?
+            if (rijIndex < 3) {BlockRowIndex = 0; }
+            else if (rijIndex < 6) {BlockRowIndex = 1;        }
+            else { BlockRowIndex = 2;}
 
-        //Nu gaan we de getallen toevoegen!
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 3; j++)
+            int rowIndexInBlok = rijIndex % 3; //Welke rij in het blok we moeten zoeken is onze eerdere index % 3.
+
+            for (int blokCol = 0; blokCol < 3; blokCol++)
             {
-                if (!blok.Cel[i, j].Locked == true)
+                for (int celRow = 0; celRow < 3; celRow++)
                 {
-                    int randomIndex = random.Next(GoedeLijstMeTMinderGetallen.Count);
-                    int willekeurigGetal = GoedeLijstMeTMinderGetallen[randomIndex];
-
-                    blok.Cel[i, j].Getalwaarde = willekeurigGetal;
-
-                    GoedeLijstMeTMinderGetallen.RemoveAt(randomIndex);
+                    int WaardeUItHetVakje = grid[blokCol, BlockRowIndex].Cel[celRow, rowIndexInBlok].Getalwaarde;
+                    Waarden.Add(WaardeUItHetVakje);
                 }
-            }
+             }
+             System.Console.WriteLine(Waarden.Count());
+            
+            // Bepaal het aantal duplicaten in de lijst
+            duplicaten = Waarden.Count - Waarden.Distinct().Count();
+            return duplicaten;
         }
-    }
-        static Blok MaakBlok(string input, int startRow, int startCol) 
+        static int EvalueerKolom(Blok[,] grid, int Kolomindex)
         {
+            List<int> Waarden = new List<int>();
+            int duplicaten = 0;
+            int BlockColIndex;
+            //Nu willen we een lijst maken van alle getallen in de verschillende blokken, in dezelfde kolommen
+            
+            //BlockColIndex bepalen: op welke kolom (vd blokken, dus 1e, 2e of 3e) moeten wij gaan zoeken?
+            if (Kolomindex < 3 + 9) {BlockColIndex = 0; }
+            else if (Kolomindex < 6 + 9) {BlockColIndex = 1;}
+            else { BlockColIndex = 2;}
 
-            Blok blok = new Blok();
+            int ColIndexInBlok = Kolomindex % 3; //Welke rij in het blok we moeten zoeken is onze eerdere index % 3.
 
+            for (int blokRow = 0; blokRow < 3; blokRow++)
+            {
+                for (int celRow = 0; celRow < 3; celRow++)
+                {
+                    int WaardeUItHetVakje = grid[BlockColIndex, blokRow].Cel[ColIndexInBlok, celRow].Getalwaarde;
+                    Waarden.Add(WaardeUItHetVakje);
+                }
+             }
+            
+            // Bepaal het aantal duplicaten in de lijst
+            duplicaten = Waarden.Count - Waarden.Distinct().Count();
+            return duplicaten;
+        }
+
+
+        static void BlokInVuller(Blok blok)
+        {
+            List<int> beschikbareGetallen = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 }; //alle getallen van 0 t/m 9!
+            //Eerst gaan we de getallen eruit halen die al locked zijn. 
+            List<int> verbodencijfers = new List<int>();
+
+            //Eerst zorgen we dat de getallen van de locked cellen niet gebruikt gaan worden. 
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    int index = (startRow + i) * 9 + (startCol + j);
-                    int getal = int.Parse(input[index].ToString());
-                    bool vergrendeld = getal != 0; //Geeft bool aan of hij vergendeld is of niet
+                    if (blok.Cel[i,j].Locked == true) {
+                        verbodencijfers.Add(blok.Cel[i,j].Getalwaarde); //hiermee maken we een lijst aan getallen die verboden zijn en dus niet mogen worden ingevuld!
+                    }
+                }
+            }
+            List<int> GoedeLijstMeTMinderGetallen = beschikbareGetallen.Except(verbodencijfers).ToList();
 
-                    blok.Cel[i, j] = new Nummer(getal, vergrendeld);
+            //Nu gaan we de getallen toevoegen!
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (!blok.Cel[i, j].Locked == true)
+                    {
+                        int randomIndex = random.Next(GoedeLijstMeTMinderGetallen.Count);
+                        int willekeurigGetal = GoedeLijstMeTMinderGetallen[randomIndex];
+
+                        blok.Cel[i, j].Getalwaarde = willekeurigGetal; //Vul de cel in het blok in!
+
+                        GoedeLijstMeTMinderGetallen.RemoveAt(randomIndex);
+                    }
+                }
+            }
+        }
+        static Blok MaakBlok(string input, int startRow, int startCol) 
+            {
+
+                Blok blok = new Blok();
+
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        int index = (startRow + i) * 9 + (startCol + j);
+                        int getal = int.Parse(input[index].ToString());
+                        bool vergrendeld = getal != 0; //Geeft bool aan of hij vergendeld is of niet
+
+                        blok.Cel[i, j] = new Nummer(getal, vergrendeld);
                 }
             }
 
             return blok;
+            }
         }
-    }
-    
-
-   internal class Nummer
-    {
-        public int Getalwaarde { get; set; }
-        public bool Locked { get; set; }
-        public Nummer(int Getal, bool JaOfNee) {
-
-            Getalwaarde = Getal;
-            Locked = JaOfNee;
-
-    }
-
-    
-    // internal class Sudoko 
-    // {
-    //     public Blok[,] Cel = new Blok[3, 3];
-
-    //     public Sudoko(string input) {
-
-
-
-    //     }
-    //     // Implementeer methoden om met de cijfers in het blok te werken
-    // }
-}
 }
--- Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
+++ Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+002664f60e3be00d292f092d95978200f948093f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03dae0f14f695e2a66ff3d79ed575d01bb67c2e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
