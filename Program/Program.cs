979c7a2d350623b32f99218cb349ead7a3f93514
RobinvanGelooven <r.m.h.vangelooven@students.uu.nl>
2023-12-12 15:45:01 +0100

    Hier nieuwe code!

--- Program/Program.cs
+++ Program/Program.cs
@@ -10,20 +10,33 @@ namespace MyApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Student Robin = new Student(5, true);
-            Console.WriteLine(Robin.nummer.ToString());
+
+            Nummer[,] Sudoku = new Nummer[9, 9];
+
         }
     }
 
    internal class Nummer
     {
-    public int Getalwaarde { get; set; }
-    public bool Locked { get; set; }
-    public Nummer(int Getal, bool JaOfNee) {
+        public int Getalwaarde { get; set; }
+        public bool Locked { get; set; }
+        public Nummer(int Getal, bool JaOfNee) {
 
-        Getalwaarde = Getal;
-        Locked = JaOfNee;
+            Getalwaarde = Getal;
+            Locked = JaOfNee;
 
     }
+
+    internal class Blok 
+    {
+        public Nummer[,] Cel = new Nummer[3, 3];
+        // Implementeer methoden om met de cijfers in het blok te werken
     }
+
+    internal class Grid 
+    {
+        public Blok[,] Cel = new Blok[3, 3];
+        // Implementeer methoden om met de cijfers in het blok te werken
+    }
+}
 }
