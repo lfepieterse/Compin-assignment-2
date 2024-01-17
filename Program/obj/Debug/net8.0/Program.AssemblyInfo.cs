138044fcd2fd01870b4ee8b5ea879c9a0f2deb40
RobinvanGelooven <r.m.h.vangelooven@students.uu.nl>
2024-01-17 17:11:55 +0100

    neem

--- Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
+++ Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32c5f95aeb0c4b56a3872c574fc9f8bce96624e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d218b36c158bc84ec36226c379862df82969da7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
